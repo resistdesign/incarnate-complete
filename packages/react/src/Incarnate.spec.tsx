0ded2f326cfc31b7e5b18a981e5f28e30f1239ad
Ryan Graff <ryantgraff@resistdesign.com>
2020-09-20 13:35:43 -0400

    test: (react Incarnate) Add a test for onIncarnateInstanceRef.

--- packages/react/src/Incarnate.spec.tsx
+++ packages/react/src/Incarnate.spec.tsx
@@ -8,6 +8,21 @@ const suite = {
         const inc = render(<Incarnate/>);
 
         expect(inc).to.be.ok();
+    },
+    'should call the instance ref handler': () => {
+        const incName = 'INC_REF_TEST';
+
+        let incRef;
+
+        render(<Incarnate
+            name={incName}
+            onIncarnateInstanceRef={ic => incRef = ic}
+        />);
+
+        expect(incRef).to.be.ok();
+        expect(incRef).to.have.property('subMap');
+        expect(incRef).to.have.property('name');
+        expect((incRef as any).name).to.eql(incName);
     }
 };
 
