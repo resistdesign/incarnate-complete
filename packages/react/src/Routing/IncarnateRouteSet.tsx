027757cb89868ea5cc7a9961013074288a9f66e9
Ryan Graff <ryantgraff@resistdesign.com>
2020-09-24 15:50:26 -0400

    refactor: (react) Remove all circular dependencies.

--- packages/react/src/Routing/IncarnateRouteSet.tsx
+++ packages/react/src/Routing/IncarnateRouteSet.tsx
@@ -1,5 +1,6 @@
 import React, { FC, PropsWithChildren } from 'react';
-import { IncarnateRedirect, IncarnateRoute } from '../.';
+import { IncarnateRedirect } from './IncarnateRedirect';
+import { IncarnateRoute } from './IncarnateRoute';
 
 export type IncarnateRouteSetProps = {
   defaultSubPath?: string;
